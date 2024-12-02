fb0021c7c1957eb783c5454978c0ab3100720098
Rajin Al Maruf <rajinalmarufananda@gmail.com>
2024-12-02 23:45:08 +0600

    login function added

--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -9,6 +9,8 @@ import {
 
 const LoginSignup = () => {
 
+    const [page, setPage] = useState(1)
+
     const [loginData, setLoginData] = useState({
         email: "",
         password: ""
@@ -24,10 +26,30 @@ const LoginSignup = () => {
         }))
     }
 
+    function loginPage(){
+        setPage(page + 1)
+        setLoginData({})
+    }
+
+    function signupPage(){
+        setPage(page - 1)
+        setLoginData({})
+    }
+
     const auth = getAuth()
 
-    function handleSubmit() {
+    function handleSignUp() {
         createUserWithEmailAndPassword(auth, loginData.email, loginData.password)
+        .then((response) => {
+            console.log(response.user)
+        })
+        .catch((error) => {
+            alert(error.message)
+        })
+    }
+
+    function handleLogin() {
+        signInWithEmailAndPassword(auth, loginData.email, loginData.password)
         .then((response) => {
             console.log(response.user)
         })
@@ -38,7 +60,9 @@ const LoginSignup = () => {
 
   return (
     <div className=''>
-      <div className='flex flex-col max-w-sm m-auto mt-20 p-10 border '>
+
+      {page===1 && 
+      (<div className='flex flex-col max-w-sm m-auto mt-20 p-10 border '>
         <p className='m-auto text-2xl mb-6 font-medium'>SignUp</p>
         <input 
             className='rounded-md p-2 my-2 border focus:outline-none text-gray-700 text-sm '
@@ -59,11 +83,51 @@ const LoginSignup = () => {
         <button 
             className='mt-4 bg-gray-800 p-2 rounded-md text-white hover:bg-gray-900'
             type="submit"
-            onClick={handleSubmit}
+            onClick={handleSignUp}
             >
                 Signup
         </button>
-      </div>
+        <div className='flex m-auto mt-6 text-sm '>
+            <p className=''>Already have an account?</p>
+            <button className='ml-2' onClick={loginPage}>Login</button>
+        </div>
+        
+        
+      </div>)}
+
+      {page===2 && 
+      (<div className='flex flex-col max-w-sm m-auto mt-20 p-10 border '>
+        <p className='m-auto text-2xl mb-6 font-medium'>Login</p>
+        <input 
+            className='rounded-md p-2 my-2 border focus:outline-none text-gray-700 text-sm '
+            type="email" 
+            name="email" 
+            onChange={handleChange}
+            value={loginData.email}
+            placeholder='Email address'
+        />
+        <input 
+            className='rounded-md p-2 my-2 border focus:outline-none text-gray-700 text-sm'
+            type="password" 
+            name="password" 
+            onChange={handleChange}
+            value={loginData.password}
+            placeholder='Password'
+        />
+        <button 
+            className='mt-4 bg-gray-800 p-2 rounded-md text-white hover:bg-gray-900'
+            type="submit"
+            onClick={handleLogin}
+            >
+                Login
+        </button>
+        <div className='flex m-auto mt-6 text-sm '>
+            <p className=''>Doesn't have an account?</p>
+            <button className='ml-2' onClick={signupPage}>SignUp</button>
+        </div>
+        
+        
+      </div>)}
       
     </div>
   )
